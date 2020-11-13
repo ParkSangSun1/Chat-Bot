e1b06ae6387cada2d8d15c169138835c3aaf0cb3
ParkSangSun1 <appleasz1028102@gmail.com>
2020-11-13 20:51:07 +0900

    봇 대화 메인창, 기능 구현

---
+++ app/src/main/java/com/example/chatbot/utils/BotResponse.kt
@@ -0,0 +1,97 @@
+package com.example.chatbot.utils
+
+import com.example.chatbot.utils.Constants.OPEN_GOOGLE
+import com.example.chatbot.utils.Constants.OPEN_SEARCH
+import java.sql.Date
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+
+object BotResponse {
+
+    fun basicResponses(_message: String): String {
+
+        val random = (0..2).random()
+        val message =_message.toLowerCase()
+
+        return when {
+
+            //Flips a coin
+            message.contains("flip") && message.contains("coin") -> {
+                val r = (0..1).random()
+                val result = if (r == 0) "heads" else "tails"
+
+                "I flipped a coin and it landed on $result"
+            }
+
+            //Math calculations
+            message.contains("solve") -> {
+                val equation: String? = message.substringAfterLast("solve")
+                return try {
+                    val answer = SolveMath.solveMath(equation ?: "0")
+                    "$answer"
+
+                } catch (e: Exception) {
+                    "Sorry, I can't solve that."
+                }
+            }
+
+            //Hello
+            message.contains("hello") -> {
+                when (random) {
+                    0 -> "Hello there!"
+                    1 -> "Sup"
+                    2 -> "Buongiorno!"
+                    else -> "error" }
+            }
+
+            //How are you?
+            message.contains("how are you") -> {
+                when (random) {
+                    0 -> "I'm doing fine, thanks!"
+                    1 -> "I'm hungry..."
+                    2 -> "Pretty good! How about you?"
+                    else -> "error"
+                }
+            }
+
+            //What time is it?
+            message.contains("time") && message.contains("?")-> {
+                val timeStamp = Timestamp(System.currentTimeMillis())
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm")
+                val date = sdf.format(Date(timeStamp.time))
+
+                date.toString()
+            }
+
+            //Open Google
+            message.contains("구글") && message.contains("시작해")-> {
+                OPEN_GOOGLE
+            }
+            message.contains("google") && message.contains("시작해")-> {
+                OPEN_GOOGLE
+            }
+            message.contains("구글") && message.contains("열어줘")-> {
+                OPEN_GOOGLE
+            }
+            message.contains("google") && message.contains("시작해")-> {
+                OPEN_GOOGLE
+            }
+
+            //Search on the internet
+            message.contains("search")-> {
+                OPEN_SEARCH
+            }
+
+            //When the programme doesn't understand...
+            else -> {
+                when (random) {
+                    0 -> "이해하지 못했습니다.."
+                    1 -> "무슨 말씀이시나요?"
+                    2 -> "알아듣지 못하겠어요"
+                    3 -> "이 말은 학습하지 못했어요"
+                    else -> "에러"
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chatbot/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.chatbot.utils
+
+object Constants {
+    const val SEND_ID ="SEND_ID"
+    const val RECEIVE_ID ="RECEIVE_ID"
+
+    const val OPEN_GOOGLE="Google 시작중..."
+    const val OPEN_SEARCH="검색중..."
+}
